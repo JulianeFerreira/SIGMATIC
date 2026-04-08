08212160126a9e96c0d84f19bfd82a69a603c7c9
Rodrigo Bianchini <rodrigo.bianchini1995@gmail.com>
2026-04-08 11:04:46 -0300

    feat: migra módulo planejamento completo

--- apps/planejamento/petic/apps.py
+++ apps/planejamento/petic/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.planejamento.pei'
+    name = 'apps.planejamento.petic'
+
--- backend_django/app_compras/apps/planejamento/pca/apps.py
+++ backend_django/app_compras/apps/planejamento/pca/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.planejamento.pei'
+    name = 'apps.planejamento.pca'
