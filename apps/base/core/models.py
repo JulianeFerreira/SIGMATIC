2df2fb9ca31c869d42ea50e46ab7d928812b5227
saulo.borges <saulo.borges@sesp.pr.gov.br>
2026-06-16 10:13:56 -0300

    getway

--- apps/base/core/models.py
+++ apps/base/core/models.py
@@ -2,19 +2,14 @@
 from django.db import models
 
 class ModuloSistema(models.Model):
-    # O nome bonito do módulo (ex: "Módulo de Compras")
     nome = models.CharField(max_length=100, verbose_name="Nome do Módulo")
     
-    # O nome que vai na URL (ex: "compras", "licitacoes")
     slug = models.SlugField(unique=True, help_text="Nome usado na URL, sem espaços.")
     
-    # O endereço real de onde o módulo está rodando (ex: http://127.0.0.1:8001)
     url_destino = models.URLField(verbose_name="URL de Destino")
     
-    # Um "interruptor" para ligar e desligar o módulo
     ativo = models.BooleanField(default=True, verbose_name="Módulo Ativo?")
     
-    # Data de registro (apenas para controle)
     criado_em = models.DateTimeField(auto_now_add=True)
 
     class Meta:
