dc8e3e78bae5a70429ebb3c8ef6dd6ede822233c
saulo.borges <saulo.borges@sesp.pr.gov.br>
2026-06-23 10:46:42 -0300

    barra-de-navegação

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -1,13 +1,19 @@
-# apps/base/urls.py
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    # 1. Rotas estáticas
     path('', views.menu_principal, name='menu_principal'),
     path('pesquisa/', views.pesquisa, name='pesquisa'),
     
+    # =======================================================
+    # 2. O SISTEMA Contratações
+    # =======================================================
+    path('', include('apps.base.core.urls')),
+    
+    # =======================================================
+    # 3. ROTAS DINÂMICAS
+    # =======================================================
     path('<str:modulo>/<str:submodulo>/', views.carrega_submodulo_dinamico, name='carrega_submodulo_dinamico'),
     path('<str:modulo>/<str:nivel2>/<str:submodulo>/', views.carrega_submodulo_dinamico_profundo, name='carrega_submodulo_dinamico_profundo'),
-
-    path('', include('apps.base.core.urls')),
 ]
