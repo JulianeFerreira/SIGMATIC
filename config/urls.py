b554548c9b7981e2bcc1dce7cff219f863507ce0
rodrigobianchini1995-hub <rodrigo.bianchini1995@gmail.com>
2026-07-28 11:31:24 -0300

    Atualização do modulo contratos (#20)

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path('governanca/governanca_dados/', include('apps.governanca.governanca_dados.urls')),
     path('planejamento/pdtic/', include('apps.planejamento.pdtic.urls')),
     path('planejamento/pca/', include('apps.planejamento.pca.urls')),
-    path('', include('apps.base.urls')),
     
+    # Módulo de Contratações - Contratos
+    path('contratacoes/contratos/', include('apps.contratacoes.contratos.urls')),
+    
+    # Rota raiz (deve ficar por último)
+    path('', include('apps.base.urls')),
 ]
