83a0fb8e7fac67f796385496d6fcf3fe540615e3
Juliane Ferreira <54564103+JulianeFerreira@users.noreply.github.com>
2026-07-30 09:51:01 -0300

    Feature/devju (#23)
    * Fix: Menu superior global via Context Processor
    
    * Fix: Menu superior global via Context Processor
    
    * fix: corrige rota de templates e ativa context_processor do menu superior global
    
    * style: Inserido ícones nos cards de métricas
    
    * style: Ajustes de box-shadow nos arquivos da pasta compras
    
    * feat: Ajuste de navegação de abas nos templates de Compras e Governanca
    
    * refactor(governanca): altera nome do modulo de governanca_dados para controle_de_acesso
    
    * feat(dashboard): adiciona biblioteca chart.umd.js local e ajusta grafico anual
    
    * wip: salvando alteracoes locais do controle de acesso
    
    * feat: sincroniza modulo governanca_dados a partir do upstream
    
    * refactor: consolida controle_de_acesso e remove pendencias do governanca_dados
    
    * Add files via upload
    
    refactor: consolida controle_de_acesso e remove pendencias do governanca_dados
    
    * wip: ajusta urls de controle de acesso
    
    ---------
    
    Co-authored-by: Juliane Ferreira de Melo <terc.julianefmelo@esesp69733.SESP>
    Co-authored-by: terc.julianefmelo <terc.julianefmelo@SESP.SMB4.PARANA>
    Co-authored-by: Juliane Ferreira <terc.julianefmelo@sesp.pr.gov.br>

--- apps/governanca/controle_de_acesso/urls.py
+++ apps/governanca/controle_de_acesso/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('dashboard/', views.dashboard_acessos_view, name='dashboard_acesso'),
     
     # Solicitações (Comentado temporariamente)
-    # path('nova_solicitacao/', views.nova_solicitacao_view, name='nova_solicitacao'),
+     path('nova_solicitacao/', views.nova_solicitacao_view, name='nova_solicitacao'),
     
     # Rotas para a equipe da CTIC/Governança (Comentado temporariamente)
-    # path('painel_solicitacoes/', views.painel_solicitacoes_view, name='painel_solicitacoes'),
-    # path('atualizar_status/<uuid:pk>/', views.atualizar_status_solicitacao, name='atualizar_status_solicitacao'),
+     path('painel_solicitacoes/', views.painel_solicitacoes_view, name='painel_solicitacoes'),
+     path('atualizar_status/<uuid:pk>/', views.atualizar_status_solicitacao, name='atualizar_status_solicitacao'),
 ]
